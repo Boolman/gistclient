913d828224a3cfd641b0d6ef2bf41b716600fecf
Emil Öhgren <boolman@gmail.com>
2018-06-18 16:15:10 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gistclient",
-    version="0.0.4",
+    version="0.0.5",
     author="boolman",
     author_email="boolman@gmail.com",
     description="GistClient",
